77edd873d5bd551ae3fd42644248442bbda65dd6
TWiedRW <61435221+TWiedRW@users.noreply.github.com>
2025-02-10 23:57:54 -0600

    Updated data to include both sides of a sphere (sets 1 and 2)

--- R/experimentData.R
+++ R/experimentData.R
@@ -3,7 +3,7 @@ if(F){
   set.seed(2026)
 
   data1 <- generate_heatmap_data(f = \(x,y)(sqrt(7^2-(x-mean(x))^2-(y-mean(y))^2)), stimuli = stimuli)
-  data2 <- generate_heatmap_data(f = \(x,y)(sqrt(7^2-(x-mean(x))^2+(y-mean(y))^2)), stimuli = stimuli)
+  data2 <- generate_heatmap_data(f = \(x,y)(sqrt(7^2+(x-mean(x))^2+(y-mean(y))^2)), stimuli = stimuli)
   data3 <- generate_heatmap_data(f = \(x,y)(x), stimuli = stimuli)
   data4 <- generate_heatmap_data(f = \(x,y)(x+y), stimuli = stimuli)
 
@@ -12,8 +12,3 @@ if(F){
   usethis::use_data(data3, overwrite = T)
   usethis::use_data(data4, overwrite = T)
 }
-
-
-
-
-
