d17400efcfa9e2155134108a40ac79a2094ce2b7
TWiedRW <61435221+TWiedRW@users.noreply.github.com>
2025-03-31 23:50:01 -0500

    Updates

--- shiny-apps/experiment-heat3d/app.R
+++ shiny-apps/experiment-heat3d/app.R
@@ -4,9 +4,10 @@
 #| Informed concent, instructions, practice, end page
 #| Save number of clicks on 3dd charts
 #| Remove 3dp for online-only participants
-#| Add confidence question after each chart group
-#|    Note -
+#| Add gradient scale to 2dd
 #|
+
+
 
 # ---- MINOR TO-DO ----
 #| Placement of helper plot
@@ -14,10 +15,28 @@
 #| Render graphs only when the grouping changes
 #|    This should help with 3dd resetting every time, but
 #|    I am skeptical since the entire experiment page is
-#|    created with renderUI.
+#|    created with renderUI. One solution is to save the
+#|    user matrix and use it every time, but this might
+#|    create a small lag when the rgl loads in for each
+#|    queston.
 #| Change initial viewing angle for 3dd
+#| Replace 3dp images with better ones using actual charts
+#| Put black border around 2dd and 3dd outputs
+#|    Not necessary, but I think it would look nice
+#| Confidence question does not appear for last set of graphs
+#|    Idea - create another variable measuring if the experiment
+#|    is complete or not. Then, the logic is that if the group
+#|    of charts changed or if experiment is completed
+#| Remove number above slider
+#|    I think that keeping the 0 and 100 at the ends would
+#|    make sense given the phrasing of the question
+#| Slider bugged? Sometimes fill does not line up with where
+#|    the icon is, but seems to be better now that steps
+#|    are 0.1 instead of 0.01
+#| 218 vs. non-218 versions (completion codes, etc. )
+#| Font sizes for value definitions
 #|
-#|
+
 
 
 # ---- Idea list ----
@@ -25,10 +44,10 @@
 #|    As of now, I can get it working for all trials except
 #|    the first trial. I suspect it is because the update
 #|    function runs before the initial experiment loads in
+#| The app looks incredibly dull. I would like to have
+#|    literally anything else to make it more exciting.
+#|    For example, color in the tab bar or background colors
 #|
-
-
-
 
 
 
@@ -82,8 +101,11 @@ ui_consent <- fluidPage(
                     label = 'Are you currently enrolled in Stat 218?',
                     choices = c("Please pick one of the following" = "",
                                 "Yes, I am a Stat 218 student" = "TRUE",
-                                "No, I am not a Stat 218 student" = "FALSE")),
-        'Informed Consent will go here',
+                                "No, I am not a Stat 218 student" = "FALSE"),
+                    selected = '', selectize = T),
+        conditionalPanel('input.is_218_student == True && input.is_218_student!=""', includeHTML('informed-consent/graphics-consent-218.html')),
+        conditionalPanel('input.is_218_student == False && input.is_218_student!=""', includeHTML('informed-consent/graphics-consent-dept.html')),
+
         radioButtons('data_consent',
                      'Select "Yes" if you agree to the informed concent and you agree to let us collect your data',
                      choices = c('Yes, I agree' = "TRUE", 'No, I do not agree' = "FALSE"),
@@ -93,6 +115,11 @@ ui_consent <- fluidPage(
     )
   )
 )
+
+
+
+
+
 
 # ---- Demographics ----
 options_ages <- c("", "Under 19", "19-25", "26-30",
@@ -153,14 +180,26 @@ ui_demographics <- fluidPage(
 
 ui_instructions <- fluidPage(
   column(width = 8, offset = 2,
+   wellPanel(
     h2('Instructions'),
     p('In this experiment, you will be using various mediums of data visualization to compare two values.
       The experiment interface will provide you with a digital rendering of a data visualizaiton, or prompt you to use one of the 3D-printed graphs.'),
-
+    p('More text will go here, plus pictures...'),
+    actionButton('submit_start_exp', 'Start Experiment')
+  )),
+  fluidRow(
+    sidebarLayout(
+      sidebarPanel(
+        p('test')
+      ),
+      mainPanel(
+        p('test')
+      )
+    )
   )
 )
 
-# ---- Practice ----
+# ---- Practice (currently inactive) ----
 
 ui_practice <- fluidPage(
   actionButton('submit_start_exp', 'Start Experiment')
@@ -175,18 +214,27 @@ ui_experiment <- fluidPage(
 )
 
 # ---- Ending page ----
-
+ui_wrapup <- fluidPage(
+  column(width = 8, offset = 2,
+    h2('Experiment complete'),
+    p('Thank you for completing the experiment.
+      Your completion code is below.'),
+    p('More text about saving code to Canvas'),
+    textOutput('user_completion_code')
+  )
+)
 
 
 # ---- UI Navigation ----
 ui <- navbarPage(
   '3D Graphics Study',
+  id = 'navpage',
   tabPanel('Informed Consent', ui_consent),
   tabPanel('Demographics', ui_demographics),
   tabPanel('Instructions', ui_instructions),
-  tabPanel('Practice', ui_practice),
+  # tabPanel('Practice', ui_practice), # Disabled for now
   tabPanel('Experiment', ui_experiment),
-  tabPanel('Wrap-up'),
+  tabPanel('Wrap-up', ui_wrapup),
   tabPanel('Developer', uiOutput('dev_page'))
 )
 
@@ -229,6 +277,8 @@ server <- function(input, output, server) {
     message(glue('\tdata_consent: {appValues$data_consent}'))
     message(glue('\tcompletion_code: {appValues$completion_code}'))
 
+    updateNavbarPage(inputId = 'navpage', selected = 'Demographics')
+
   })
 
   # ---- Demographics logic ----
@@ -259,11 +309,17 @@ server <- function(input, output, server) {
     message(glue('\tuser_pre_hash: {appValues$user_pre_hash}'))
     message(glue('\tuser_id: {appValues$user_id}'))
 
-    #Maybe use app start time too in user_id?
+    # Maybe use app start time too in user_id?
     message(glue('A new user was created at {appValues$appStartTime}: {appValues$user_id}'))
-  })
-  # ---- Instructions logic ----
 
+    # Update page
+    updateNavbarPage(inputId = 'navpage', selected = 'Instructions')
+
+})
+  # ---- Instructions logic ----
+  observeEvent(input$submit_start_exp, {
+    updateNavbarPage(inputId = 'navpage', selected = 'Experiment')
+  })
 
   # ---- Practice logic ----
 
@@ -284,13 +340,16 @@ server <- function(input, output, server) {
     user_slice = NULL,
     user_guess_smaller = NULL,
     user_guess_slider = NULL,
-    data_consent = NULL
+    data_consent = NULL,
+    user_last_trial = FALSE
   )
 
 
 
 
   observeEvent(input$submit_start_exp, {
+
+
     #User information
     expValues$user_id <- appValues$user_id
     expValues$data_consent <- appValues$data_consent
@@ -350,9 +409,10 @@ server <- function(input, output, server) {
             'set1' = render_3dd('../../print-files/rgl-data1-base.stl',
                                 '../../print-files/rgl-data1-bars.stl',
                                 '../../print-files/rgl-data1-letters.stl'),
-            'set2' = render_3dd('../../print-files/rgl-data2-base.stl',
-                                '../../print-files/rgl-data2-bars.stl',
-                                '../../print-files/rgl-data2-letters.stl')
+            #MAJOR CHANGE: NEED TO CONVERT BACK TO data2 WHEN FILES ARE CREATED
+            'set2' = render_3dd('../../print-files/rgl-data1-base.stl',
+                                '../../print-files/rgl-data1-bars.stl',
+                                '../../print-files/rgl-data1-letters.stl')
     )
     rglwidget()
   })
@@ -376,7 +436,12 @@ server <- function(input, output, server) {
   output$experiment_plot <- renderUI({
     switch (expValues$user_slice$media,
             '2dd' = plotOutput('plot_2dd'),
-            '3dd' = rglwidgetOutput('plot_3dd'),
+            '3dd' = tagList(
+              rglwidgetOutput('plot_3dd'),
+              helpText('You may use your mouse to move the chart on the screen.'),
+              helpText('Right click: move the plot.'),
+              helpText('Scroll: zoom in and out.')
+            ),
             '3dp' = tagList(
               h3('Use the 3D chart that looks like the image below.'),
               imageOutput('plot_3dp')
@@ -400,8 +465,8 @@ server <- function(input, output, server) {
           h2(glue('Trial {expValues$user_trial_num} of {expValues$user_trial_max}')),
           h3(glue('Group {expValues$user_set_num} of {expValues$user_set_max}')),
           p(glue('Use the following definitions for Trial {expValues$user_trial_num}. If you need help identifying the values on the chart, there is an option at the bottom of this section to help.')),
-          h4(glue('Value 1: {expValues$user_slice$p1}')),
-          h4(glue('Value 2: {expValues$user_slice$p2}')),
+          p(glue('Value 1: {expValues$user_slice$p1}')),
+          p(glue('Value 2: {expValues$user_slice$p2}')),
 
           radioButtons('user_guess_smaller',
                        'Which of the following values is larger?',
@@ -409,7 +474,7 @@ server <- function(input, output, server) {
                        selected = ''),
           sliderInput('user_guess_slider',
                       'If the larger value you selected above represents 100 units, how many units does the smaller value represent?',
-                      min = 0, max = 100, value = 50, ticks = F, step = 0.01),
+                      min = 0, max = 100, value = 50, ticks = F, step = 0.1),
 
           actionButton('submit_user_trial',
                        'Submit'),
@@ -435,15 +500,21 @@ server <- function(input, output, server) {
     #Update trial number and trial slice
     if(expValues$user_trial_num < expValues$user_trial_max){
       expValues$user_trial_num <- expValues$user_trial_num + 1
+
       expValues$user_slice <- dplyr::filter(expValues$user_results, user_trial_order == expValues$user_trial_num)
 
-      if(expValues$user_set_num != expValues$user_slice$user_set_order){
+      if(expValues$user_set_num != expValues$user_slice$user_set_order | expValues$user_last_trial == T){
         # Alt phrasing ideas: https://www.marquette.edu/student-affairs/assessment-likert-scales.php
         showModal(modalDialog(
           radioButtons('user_confidence',
-                       'Rate the confidence of your answers for the previous group of questions.',
-                       choices = c('1 - Not confident', '2 - Slightly confident', '3 - Somewhat confident', '4 - Moderately confident', '5 - Extremely confident')),
-          actionButton('submit_confidence', 'Submit'),
+                       glue('Rate the confidence of your answers for the previous group of questions (Group {expValues$user_set_num}).'),
+                       choices = c('1 - Not confident',
+                                   '2 - Slightly confident',
+                                   '3 - Somewhat confident',
+                                   '4 - Moderately confident',
+                                   '5 - Extremely confident'),
+                       selected = ''),
+          actionButton('submit_confidence', 'Submit', disabled = T),
           title = 'Some title here I guess',
           footer = NULL
 
@@ -451,11 +522,16 @@ server <- function(input, output, server) {
       }
 
       expValues$user_set_num <- expValues$user_slice$user_set_order
+      # If next trial is last trial, create marker for confidence question
+      if(expValues$user_trial_num == expValues$user_trial_max){
+        expValues$user_last_trial <- TRUE
+      }
     } else if(expValues$user_trial_num  == expValues$user_trial_max){
+
       # Move to ending page
-      # ...
       message(glue('The following user has completed the experiment!'))
       message(glue('\t{appValues$user_id}'))
+      updateNavbarPage(inputId = 'navpage', selected = 'Wrap-up')
     } else {
       message('Trial number exeeds maximun number of trials. What happened???')
     }
@@ -469,16 +545,28 @@ server <- function(input, output, server) {
 
   })
 
+  # Enable confidence submit button when choice is selected
+  observeEvent(input$user_confidence, {
+    updateActionButton('submit_confidence',
+                       session = getDefaultReactiveDomain(),
+                       disabled = F)
+  })
+
+
   observeEvent(input$submit_confidence, {
     # Save confidence
     # ...
-    validate(need(input$user_confidence, 'You must select your confidence before continuing.'))
+
+    #Validate does not display message, but will work
+    # validate(need(input$user_confidence, 'You must select your confidence before continuing.'))
     removeModal()
   })
 
 
   # ---- Post-experiment logic ----
-
+  output$user_completion_code <- renderText({
+    appValues$completion_code
+  })
 
 
   # ---- Developer logic ----
